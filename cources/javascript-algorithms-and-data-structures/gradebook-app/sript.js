2df886edc63e2050c7faff62cb15e8318e41cf09
Arunjith T S <arunjith.unofficial@gmail.com>
2024-06-09 00:16:50 +0530

    added function to get grade

--- cources/javascript-algorithms-and-data-structures/gradebook-app/sript.js
+++ cources/javascript-algorithms-and-data-structures/gradebook-app/sript.js
@@ -1,12 +1,31 @@
 function getAverage(scores) {
-    let sum = 0;
+  let sum = 0;
 
-    for (const score of scores) {
-      sum += score;
-    }
-  
-    return sum / scores.length;
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
 }
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+function getGrade(score) {
+  let grade;
+  if (score == 100) {
+    grade = "A++";
+  } else if (score > 89) {
+    grade = "A";
+  } else if (score > 79) {
+    grade = "B";
+  } else if (score > 69) {
+    grade = "C";
+  } else if (score > 59) {
+    grade = "D";
+  } else {
+    grade = "F";
+  }
+  return grade;
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
